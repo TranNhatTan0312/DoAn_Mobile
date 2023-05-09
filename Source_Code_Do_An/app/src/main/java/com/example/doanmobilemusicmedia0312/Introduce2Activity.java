e371c546b0afa107707c404c1957d7fc22ff80b3
Tran Nhat Tan <86831854+TranNhatTan0312@users.noreply.github.com>
2023-05-09 17:17:24 +0700

    up

---
+++ Source_Code_Do_An/app/src/main/java/com/example/doanmobilemusicmedia0312/Introduce2Activity.java
@@ -0,0 +1,28 @@
+package com.example.doanmobilemusicmedia0312;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Introduce2Activity extends AppCompatActivity {
+
+    Button btn_start2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_choosemode);
+
+        btn_start2 = findViewById(R.id.btn_started);
+        btn_start2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
