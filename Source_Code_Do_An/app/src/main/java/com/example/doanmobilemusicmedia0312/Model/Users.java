b9793142bbfed5bc9830366e39439e045f312773
Tran Nhat Tan <86831854+TranNhatTan0312@users.noreply.github.com>
2023-06-18 16:04:36 +0700

    Update class logo, xu li dang nhap

---
+++ Source_Code_Do_An/app/src/main/java/com/example/doanmobilemusicmedia0312/Model/Users.java
@@ -0,0 +1,81 @@
+package com.example.doanmobilemusicmedia0312.Model;
+
+
+public class Users {
+    private String FullName, email, Phone, Password, Avatar, Gender, Birth;
+    public Users(String fullName, String email,String password) {
+        FullName = fullName;
+        this.email = email;
+        Phone = "";
+        Password = password;
+        Avatar = "";
+        Gender = "";
+        Birth = "";
+    }
+    public Users(String fullName, String email, String phone, String password, String avatar, String gender, String birth) {
+        FullName = fullName;
+        this.email = email;
+        Phone = phone;
+        Password = password;
+        Avatar = avatar;
+        Gender = gender;
+        Birth = birth;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String fullName) {
+        FullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getAvatar() {
+        return Avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        Avatar = avatar;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public String getBirth() {
+        return Birth;
+    }
+
+    public void setBirth(String birth) {
+        Birth = birth;
+    }
+}
+
